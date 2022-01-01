247fd849cf68a5f4c220bb837d81ab9959840b74
NanaKweku <nanakwekudev@gmail.com>
2022-01-01 21:52:18 +0000

    oop

---
+++ constructors_&_'this'_keyword/app.js
@@ -0,0 +1,20 @@
+//creating a constructor
+
+function userdata(username ,password){
+    this.userId=username;
+    this.userPass=password;
+
+    
+}
+
+
+const abozigi = new userdata('Abozigi', `kicking123@`);
+
+const richard = new userdata('Baddest419','flybwouy3310');
+
+
+
+// console.log(abozigi);
+
+
+// console.log(richard);
