4c11e07817f91ce2a1a38d1ee1b097016350a6c8
NanaKweku <nanakwekudev@gmail.com>
2022-01-02 23:43:45 +0000

    used basics of constructors to create anage calculating app

--- constructors_&_'this'_keyword/app.js
+++ constructors_&_'this'_keyword/app.js
@@ -1,15 +1,39 @@
-//creating a constructor
 
-function person(fname,dob){
-    this.firstname=fname;
-    this.birthday= new Date(dob);
 
-    //creating a method in the constructor.  An object is 
-    const mymethodGetDate = function(){
-  
+
+//agecalculator constructor
+
+function getAge(dob){
+  this.birthday = new Date(dob);
+
+  this.calculateAge= function(){
+      const diff = Date.now()-this.birthday.getFullYear();
+      const ageDate = new Date(diff);
+      const year = ageDate.getUTCFullYear()-this.birthday.getFullYear();
+      return Math.abs(year)
+
     }
 }
 
-const petersDet =new person('peter','06/30/1999');
+ const kwekusAge= new getAge('09-12-2004')
 
-console.log(petersDet);
+ console.log(kwekusAge.calculateAge());
+
+
+ //getting ui values
+
+ const form = document.querySelector('.age-form'),
+       dateofbirth = document.querySelector('.dateofbirth'),
+       checkageBtn = document.querySelector('.check-age'),
+       ageUi= document.querySelector('.age');
+
+form.addEventListener('submit',function(e){
+
+  const userage=new getAge(dateofbirth.value);
+  ageUi.value= userage.calculateAge();
+  e.preventDefault();
+});
+
+
+       
+
