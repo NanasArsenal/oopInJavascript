94910f8379c6c45f99877ef9b92e95e0b9402247
NanaKweku <nanakwekudev@gmail.com>
2022-01-27 20:29:57 +0000

    first js oop project

---
+++ projects/BooklistApp/app.js
@@ -0,0 +1,22 @@
+//Book Constructor
+function  Book(title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn ;
+}
+
+
+//UI Constructor
+function UI(){}
+
+
+//Events listeners
+document.getElementById('book-form').addEventListener('submit',
+ function(e){
+   //get form fields
+    const title = document.getElementById()
+
+
+    e.preventDefault();
+
+})
