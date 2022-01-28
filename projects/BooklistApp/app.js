8c84a8ead6bc78286ea369248a0c9a96d241242c
NanaKweku <nanakwekudev@gmail.com>
2022-01-28 11:05:19 +0000

    I created the addBOOKLIST PROTOTYPE

--- projects/BooklistApp/app.js
+++ projects/BooklistApp/app.js
@@ -9,14 +9,32 @@ function  Book(title, author, isbn){
 //UI Constructor
 function UI(){}
 
+// ADD BOOK TO LIST  
+UI.prototype.addBookList = function(thebook){
+    console.log(thebook);
+}
+
+
 
 //Events listeners
 document.getElementById('book-form').addEventListener('submit',
+
  function(e){
    //get form fields
-    const title = document.getElementById()
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value;  
+
+    //Creating a instance of the Book constructor 
+    const a_book = new Book(title,author,isbn);
+
+    //Creating an instance of the UI constructor
+    const ui = new UI();
+
+    //adding book to the list
+    ui.addBookList(a_book)
+
 
 
     e.preventDefault();
-
 })
